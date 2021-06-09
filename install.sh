62acedca91177317e36e178b0883a7549886ccb2
Mustafa Çalışkan <musfay@protonmail.com>
2021-06-09 16:32:59 +0300

    improve installation script

--- install.sh
+++ install.sh
@@ -1,18 +1,19 @@
 #!/usr/bin/env bash
 # must be runned with root privileges
 
-function backup {
-	mkdir $INSTALL_PATH/backup
-	mv $INSTALL_PATH/*nix $INSTALL_PATH/backup
-}
+# I stopped backuping as i can revert my changes from git tree
+#function backup {
+#	mkdir $INSTALL_PATH/backup
+#	mv $INSTALL_PATH/*nix $INSTALL_PATH/backup
+#}
 
 function installConfiguration {
-	backup
+#	backup
 	cp *nix $INSTALL_PATH/
 }
 
 if [ -f "/mnt/etc/nixos/configuration.nix" -a -f "/mnt/etc/nixos/hardware-configuration.nix" ]; then	
-        INSTALL_PATH=/mnt/etc/nixos
+    INSTALL_PATH=/mnt/etc/nixos
 	installConfiguration
 	# regenerate hardware-config
 	nixos-generate-config --root /mnt
@@ -20,6 +21,8 @@ if [ -f "/mnt/etc/nixos/configuration.nix" -a -f "/mnt/etc/nixos/hardware-config
 	echo "\n\n      Installation finished!"
 else
 	INSTALL_PATH=/etc/nixos
-	installConfiguration 
+	installConfiguration
+	# regenerate hardware-config
+	nixos-generate-config --root /
 fi
 
