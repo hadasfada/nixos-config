5374cb05bedbb49d043fa748525098bf52d26894
Mustafa Çalışkan <musfay@protonmail.com>
2021-07-11 13:31:35 +0300

    environment: fix bash

--- environment/bash.nix
+++ environment/bash.nix
@@ -1,18 +1,19 @@
 { config, pkgs, lib, ... }:
 
 {
-  programs.bash.shellAliases = {
+  programs.bash = { 
+    shellAliases = {
       "rebuild" = "sudo nixos-rebuild switch";
       "upgrade" = "sudo nixos-rebuild --upgrade switch";
       "configRepo" = "pushd $CONFIG_PATH";
       "ls" = "ls --color=tty";
+    };
+    shellInit = ''
+      export CONFIG_PATH=$HOME/.nixos-config
+      source $CONFIG_PATH/environment/scripts/configControl.sh
+      source $CONFIG_PATH/environment/scripts/papermc.sh
+      source $CONFIG_PATH/environment/scripts/ngrok.sh
+      source $CONFIG_PATH/environment/scripts/repoControl.sh
+    '';
   };
-
-  environment.interactiveShellInit = ''
-    export CONFIG_PATH=$HOME/.nixos-config
-    source $CONFIG_PATH/environment/scripts/configControl.sh
-    source $CONFIG_PATH/environment/scripts/papermc.sh
-    source $CONFIG_PATH/environment/scripts/ngrok.sh
-    source $CONFIG_PATH/environment/scripts/repoControl.sh
-  '';
 }
