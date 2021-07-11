91e49844ffa70e58870f6bd948a7086d44d58e07
Mustafa Çalışkan <musfay@protonmail.com>
2021-07-11 13:13:42 +0300

    environment: source scripts

--- environment/bash.nix
+++ environment/bash.nix
@@ -1,6 +1,13 @@
 { config, pkgs, lib, ... }:
 
 {
+  programs.bash.shellAliases = {
+      "rebuild" = "sudo nixos-rebuild switch";
+      "upgrade" = "sudo nixos-rebuild --upgrade switch";
+      "configRepo" = "pushd $CONFIG_PATH";
+      "ls" = "ls --color=tty";
+  };
+
   environment.interactiveShellInit = ''
     export CONFIG_PATH=$HOME/.nixos-config
     source $CONFIG_PATH/environment/scripts/configControl.sh
