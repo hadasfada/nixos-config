d1c87d833b7c9e48da2d603894a88c050f2b6e00
Mustafa Çalışkan <musfay@protonmail.com>
2021-08-19 10:44:51 +0300

    system: switch to grub

--- system/boot.nix
+++ system/boot.nix
@@ -2,9 +2,15 @@
 
 {
   boot = {
-    loader.systemd-boot.enable = true;
+    loader.grub = {
+      enable = true;
+      efiSupport = true;
+      device = "/dev/sda1";
+      forceInstall = true;
+      useOSProber = true;
+    };
     loader.efi.canTouchEfiVariables = true;
-    kernelPackages = pkgs.linuxPackages_latest;
+    kernelPackages = pkgs.linuxPackages_xanmod;
     kernelParams = ["nomce" "loglevel=3" # cleaner boot
     "amdgpu.si_support=1" "radeon.si_support=0" # enable amdgpu support for hainan
     "module_blacklist=iTCO_wdt" "mitigations=off" # some tweaks to make my potato run cooler
