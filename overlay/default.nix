a29538edd1b7cad6524bb241168f641e2fcd1c72
Mustafa Çalışkan <musfay@protonmail.com>
2021-08-20 10:16:29 +0300

    overlay: format

--- overlay/default.nix
+++ overlay/default.nix
@@ -1,31 +1,5 @@
-{ config, pkgs, lib, ... }:
+{ ... }: {
 
-{
-  nixpkgs.overlays = [ (self: super:
-    with super;
-    {
-      #tok = super.libsForQt5.callPackage ./tok { };
-      mkbootimg = callPackage ./mkbootimg {};
-      sx = callPackage ./sx {
-        inherit (xorg) xauth xorgserver;
-      };
-      cubocore = recurseIntoAttrs (import ./cubocore {
-        inherit pkgs;
-        inherit (lib) makeScope;
-        inherit libsForQt5;
-      });
-      libarchive-qt = libsForQt5.callPackage ./libarchive-qt { };
-      xmake = callPackage ./xmake { };
+  nixpkgs.overlays = [ import ./overlay.nix ];
 
-      vim_configurable = vim_configurable.overrideAttrs (old: rec {
-          version = "8.2.3340";
-          src = fetchFromGitHub {
-            owner = "vim";
-            repo = "vim";
-            rev = "v${version}";
-            sha256 = "sha256-QMY2DGASAqishIiZy/jfmDxNsGftoEJbAwPtmEO9zws=";
-          };
-      });
-    }
-  )];
 }
---
+++ overlay/overlay.nix
@@ -0,0 +1,28 @@
+self: super: with super; {
+
+  tok = libsForQt5.callPackage ./tok { };
+
+  mkbootimg = callPackage ./mkbootimg {};
+  sx = callPackage ./sx {
+    inherit (xorg) xauth xorgserver;
+  };
+
+  cubocore = recurseIntoAttrs (import ./cubocore {
+    inherit pkgs;
+    inherit (lib) makeScope;
+    inherit libsForQt5;
+  });
+
+  libarchive-qt = libsForQt5.callPackage ./libarchive-qt { };
+
+  vim_configurable = vim_configurable.overrideAttrs (old: rec {
+    version = "8.2.3340";
+    src = fetchFromGitHub {
+      owner = "vim";
+      repo = "vim";
+      rev = "v${version}";
+      sha256 = "sha256-QMY2DGASAqishIiZy/jfmDxNsGftoEJbAwPtmEO9zws=";
+    };
+  });
+
+}
